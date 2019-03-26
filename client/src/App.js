d15bdf42c41d7a46aad70c499459c9f0e674e146
mcchan1 <mcchan1@gmail.com>
2019-03-26 16:14:27 -0400

    get contract addres and render

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import McCoyContract from "./contracts/McCoyContract.json";
-import IERC721 from "./contracts/IERC721.json";
+//import IERC721 from "./contracts/IERC721.json";
 import getWeb3 from "./utils/getWeb3";
 import { Container,Grid, Button, Form} from 'semantic-ui-react';
 import { APIClient, Openlaw } from 'openlaw';
 import "./App.css";
 
 class App extends Component {
-  state = { instance: undefined, web3: null, accounts: null, contract: null };
+  state = { instance: undefined, contractAddress: null,
+    web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -16,7 +17,7 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
+      console.log(accounts[0]);
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = McCoyContract.networks[networkId];
@@ -25,9 +26,22 @@ class App extends Component {
         McCoyContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
+
       this.setState({instance});
       console.log("McCoyContract instance..",this.state.instance);
-      console.log(instance.options.address);
+      const contractAddress = await instance.options.address;
+
+      this.setState({contractAddress});
+      console.log("contractAddress...",this.state.contractAddress);
+     
+      // instance.methods.name().call({from: accounts[0]}, (error, result) => 
+      //   {console.log(error, result)});
+        
+      // instance.methods.isMinter('0xad9b86640008f02d9f2f3f0702133cea4eecb18c').call({
+      //     from: accounts[0] 
+      //   }, (error, result) => {
+      //     console.log(result);
+      // }); 
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
@@ -52,8 +66,12 @@ class App extends Component {
     }
     return (
       <div className="App">
+      <Container>
         <h1>Good to Go!</h1>
+        <p>contract K {this.state.contractAddress}</p>
 
+        <Button>Submit</Button>
+        </Container>
       </div>
     );
   }
