c9671d8335f61b3d29431624f12ae164a365c8b4
mcchan1 <mcchan1@gmail.com>
2019-03-28 14:09:16 -0400

    add TokenCards as option on Landingpage
    todo - pass web3 and instance props to other components.

--- client/src/components/MintToken.js
+++ client/src/components/MintToken.js
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { Component } from "react";
 import { Button, Form, Segment } from 'semantic-ui-react'
+import McCoyContract from "../contracts/McCoyContract.json";
+import getWeb3 from "../utils/getWeb3";
 
-const MintToken = () => (
-  <Segment color = 'teal'>
-  <Form>
-    <Form.Field>
-      <label>Meta Data to Include</label>
-      <input placeholder='some string' />
-    </Form.Field>
-    <Form.Field>
-      <label>Add Token ID Number</label>
-      <input placeholder='Token Number' />
-    </Form.Field>
+class MintToken extends Component {
 
-    <Button type='submit'>Mint Token</Button>
-  </Form>
-  </Segment>
-)
+  state = {instance: undefined, contract: null, tokenId: '', metaData: ''};
+
+   componentDidMount = async () => {
+    //console.log("mint mounint...");
+      try{
+        console.log('minting component..');
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        console.log('accounts..',accounts[0]);
+
+      } catch(error) {
+        console.log("errors..",error);
+      }
+   }
+
+
+  mintToken = async (event)=> {
+    console.log('minting...',this.state.metaData, this.state.tokenId);
+    event.preventDefault();
+    // try{
+    //   const accounts = await getWeb3.eth.getAccounts();
+    //   console.log("sending from MM: " + accounts[0]);
+
+    // } catch(error) {
+    //   console.log(error);
+    // }
+
+  // instance.methods.createMcCoyToken(
+  //       this.state.metaData, this.state.tokenId).send( 
+  //       {from: accounts[0]}, (error, txHash) =>{
+  //           console.log(error, txHash);
+  //       });
+  };
+
+  render() {
+
+    return (
+      <Segment color = 'teal'>
+      <Form onSubmit = {this.mintToken}>
+        <Form.Field>
+          <label>Meta Data to Include</label>
+          <input placeholder='some string' 
+            value = {this.state.metaData}
+            onChange = {event => this.setState({metaData: event.target.value})}
+          />
+        </Form.Field>
+        
+        <Form.Field>
+          <label>Add Token ID Number</label>
+          <input placeholder='Token Number'
+          value = {this.state.tokenId}
+          onChange = {event => this.setState({tokenId: event.target.value})}
+          />
+        </Form.Field>
+
+        <Button type='submit'>Mint Token</Button>
+      </Form>
+      </Segment>
+
+    )
+  }
+}
 
 export default MintToken
