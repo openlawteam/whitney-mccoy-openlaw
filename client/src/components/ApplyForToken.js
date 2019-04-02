ecad58f8aeb60eacb91685d45f5c481b0e98ec21
mcchan1 <mcchan1@gmail.com>
2019-04-02 10:03:29 -0400

    update applyforToken -
    switch to dev instance. RC instance not loggiing in

--- client/src/components/ApplyForToken.js
+++ client/src/components/ApplyForToken.js
@@ -7,10 +7,15 @@ import 'openlaw-elements/dist/openlaw-elements.min.css';
 //var OPENLAW_EMAIL = process.env["OPENLAW_EMAIL"];
 //var OPENLAW_PASSWORD = process.env["OPENLAW_PASSWORD"];
 
-    const URL = "https://app-rc.openlaw.io/"; 
-    const TEMPLATE_NAME = "Whitney-McCoy Donor Certificate"; 
+    const URL = "https://develop.dev.openlaw.io"; 
+    const TEMPLATE_NAME = "Whitney Test"; 
     const OPENLAW_USER = 'michael.chan@consensys.net'; 
     const OPENLAW_PASSWORD = 'ShuPei2013!'; 
+    //Login testing...
+    // const URL = "https://app.openlaw.io";//https://develop.dev.openlaw.io";
+    // const TEMPLATE_NAME = "Sale Agreement - LV";//j"OpenLaw API Tutorial Sale Agreement";
+    // const OPENLAW_USER = 'michael.chan@consensys.net';
+    // const OPENLAW_PASSWORD = 'ShuPei2013!';
     
     const openLawConfig = {
       server:URL, 
@@ -27,10 +32,10 @@ class ApplyForToken extends Component {
   componentDidMount = async() => {
 
     
-    console.log('apply for token..', openLawConfig.userName, openLawConfig.password);
+    //console.log('apply for token..', openLawConfig.userName, openLawConfig.password);
     try{
-      //apiClient.login(openLawConfig.userName,openLawConfig.password).then(console.log);
-      console.log(apiClient);
+      apiClient.login(openLawConfig.userName,openLawConfig.password).then(console.log);
+      //console.log(apiClient);
     }
     catch(error){
       console.log('errors..', error);
