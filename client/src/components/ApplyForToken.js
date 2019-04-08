9b6f80aeceaf5d6f21c32e41693c223b38e1dad0
mcchan1 <mcchan1@gmail.com>
2019-04-08 13:14:32 -0400

    import OpenLaw form w/ OL Elements
    todo - find out how statelifter works

--- client/src/components/ApplyForToken.js
+++ client/src/components/ApplyForToken.js
@@ -18,71 +18,122 @@ import 'openlaw-elements/dist/openlaw-elements.min.css';
       userName:OPENLAW_USER,
       password:OPENLAW_PASSWORD
     }
- const apiClient = new APIClient(URL);
+   const apiClient = new APIClient(URL);
+     apiClient
+    .login(openLawConfig.userName,openLawConfig.password)
+    .catch((error) => {
+      if (/500/.test(error)) {
+        console.warn('OpenLaw APIClient: Please authenticate to the APIClient to use the Address input.');
+        return;
+      }
+      console.error('OpenLaw APIClient:', error);
+    });
 
 class ApplyForToken extends Component {
+
+    static defaultProps = {
+    stateLifter: () => {},
+  };
+
+  // trick eslint
+  static propTypes = {
+    stateLifter: () => {},
+  };
+
    state = {
-    myTemplate:null
+    myTemplate:null, 
+    parameters:{},
+    executionResult:{},
+    variables:[],
+    donorName:'',
+    donorEmail:'',
+    donorEthAddress:null
   };
 
   componentDidMount = async() => {
 
     try{
-      // apiClient Login 
-      apiClient
-      .login(openLawConfig.userName,openLawConfig.password)
-      .catch((error) => {
-        if (/500/.test(error)) {
-          console.warn('OpenLaw APIClient: Please authenticate to the APIClient to use the Address input.');
-          return;
-        }
-        console.error('OpenLaw APIClient:', error);
-      });
+      this.update();
 
+   //    const myCompiledTemplate = await Openlaw.compileTemplate(myContent);
+   //    this.setState({myCompiledTemplate});
+   //    console.log("my compiled template..." + myCompiledTemplate);
 
-      const myTemplate = await apiClient.getTemplate(openLawConfig.templateName);
-      this.setState({myTemplate});    
-      console.log(myTemplate.content);
-   // //pull properties off of JSON and make into variables
-   //  const myTitle = myTemplate.title;
+   //    const parameters = this.state.parameters;
+   //    console.log("parameters.." + parameters);
+   //    this.setState({parameters});
+   //    const executionResult = await Openlaw.execute(this.state.myCompiledTemplate, {}, this.state.parameters);
+   //    console.log("ex result.." + executionResult);
+   //    // this.setState({executionResult});
+   //    // const variables = await Openlaw.getExecutedVariables(this.state.executionResult,{});
+   //    // this.setState({variables});
+   //    // console.log("execute.." + this.state.variables, this.state.executionResult);
+   // // //pull properties off of JSON and make into variables
+   // //  const myTitle = myTemplate.title;
 
     } catch(error){
       console.log('errors..', error);
     }
     
   }
+  update = async (key, value) => {
+      console.log('updates..');
+      const myTemplate = await apiClient.getTemplate(openLawConfig.templateName);
+      this.setState({myTemplate});   
+      const myContent = myTemplate.content; 
+      console.log(myTemplate.content);
+
+          const updatedDraftParameters = key
+      ? ({
+        ...this.state.parameters,
+        [key]: value,
+      }) : (
+        this.state.parameters
+      );
+
+    this.setState(({parameters}) => {
+      const concatParameters = {...parameters, ...updatedDraftParameters};
+      // https://docs.openlaw.io/openlaw-object/#compiletemplate
+      const {compiledTemplate} = Openlaw.compileTemplate(myContent);
+      // https://docs.openlaw.io/openlaw-object/#execute
+
+      const {executionResult, errorMessage} = Openlaw.execute(compiledTemplate, {}, concatParameters);
+
+      if (errorMessage) {
+        // eslint-disable-next-line no-undef
+        console.error('Openlaw Execution Error:', errorMessage);
+        return;
+      }
+
+      const state = {
+        executionResult,
+        parameters: concatParameters,
+        // https://docs.openlaw.io/openlaw-object/#getexecutedvariables
+        variables: Openlaw.getExecutedVariables(executionResult, {}),
+      };
+
+      // send props up
+      this.props.stateLifter(state);
+
+      return state;
+    });
+     // const myCompiledTemplate = await Openlaw.compileTemplate(myContent);
+  }
 
   render() {
     return(
     <Container>
       <h2>powered by openlaw</h2>
-
-      <Form>
-        <Form.Field>
-          <label>First Name</label>
-          <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
-        </Form.Field>
-          <Form.Field>
-          <label>Email</label>
-          <input placeholder='Email' required/>
-        </Form.Field>
-        <Form.Field label='Yes, Ethereum Address' control='input' type='radio' name='htmlRadios' />
-        <Form.Field label='No' control='input' type='radio' name='htmlRadios' />
-        <Form.Field>
-          <label>My Ethereum Address</label>
-          <input placeholder='My Ethereum Address' />
-        </Form.Field>
-        <Form.Field>
-          <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
-      </Form>
-
-
+      {Object.keys(this.state.executionResult).length && (
+        <OpenLawForm
+          apiClient={apiClient}
+          executionResult={this.state.executionResult}
+          parameters={this.state.parameters}
+          onChangeFunction={this.update}
+          openLaw={Openlaw}
+          variables={this.state.variables}
+        /> 
+    )}
     </Container>
     )
   }//render 
