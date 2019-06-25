279d97c1f0ccd38cddfac0db3ecc8b4704f3688a
Michael Chan <mcchan1@gmail.com>
2019-06-24 21:32:12 -0400

    Updates

---
+++ precache-manifest.aea08387d427a9926c30a26205bef157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4d5c5e4535614839ce6e0963ed410e",
+    "url": "/static/media/mccoy-film-crop.da4d5c5e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f443e32722cb78c8c0eb",
+    "url": "/static/js/main.f443e327.chunk.js"
+  },
+  {
+    "revision": "2428c70c672b92265340",
+    "url": "/static/js/1.2428c70c.chunk.js"
+  },
+  {
+    "revision": "f443e32722cb78c8c0eb",
+    "url": "/static/css/main.6bb1745e.chunk.css"
+  },
+  {
+    "revision": "6026834ddb0e8fe62aa0012d5b753385",
+    "url": "/index.html"
+  }
+];
